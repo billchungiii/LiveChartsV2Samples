2e82dd988a998872878cb954e207fa12f11e4cff
Bill Chung <bill_chungiii@hotmail.com>
2023-06-23 23:47:39 +0800

    加入專案檔案。

---
+++ BindingLibrary/NotifyPropertyBase.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace BindingLibrary
+{
+    public abstract class NotifyPropertyBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (!EqualityComparer<T>.Default.Equals(storage, value))
+            {
+                storage = value;
+                OnPropertyChanged(propertyName);
+            }
+        }
+    }
+}
---
+++ LiveChartsV2Sample001/MainViewModel.cs
@@ -0,0 +1,38 @@
+﻿using BindingLibrary;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace LiveChartsV2Sample001
+{
+    public class MainViewModel : NotifyPropertyBase
+    {
+        private ObservableCollection<ISeries> _series;
+
+        public ObservableCollection<ISeries> Series
+        {
+            get => _series;
+            set => SetProperty(ref _series, value);
+        }
+
+        public MainViewModel()
+        {
+            // LiveChartsCore.SkiaSharpView.LineSeries
+
+            var items = Enumerable.Repeat(Enumerable.Range(0, 50).OrderBy(d => Guid.NewGuid()), 2).SelectMany(x => x).ToArray();
+
+            Series = new ObservableCollection<ISeries>()
+            {
+                new LineSeries<int>
+                {
+                    Values = items,
+                    Fill = null
+                },
+
+            };
+
+        }
+    }
+}
